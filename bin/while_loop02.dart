8e8522474028129c0f85a26e38629905d2f8956e
oktambek2003 <anvaralimnazarov84@gmail.com>
2022-11-02 16:10:06 +0500

    2-misol

--- bin/while_loop02.dart
+++ bin/while_loop02.dart
@@ -2,4 +2,16 @@
   Create function func with "a " argument
   Return the number of even numbers up to "a".
 */
-void main() {}
+int func(int a) {
+  int b = 0;
+
+  while (b + 4 < a) {
+    b += 2;
+    print(b);
+  }
+  return b +2;
+}
+
+void main() {
+  print(func(12));
+}
