400af7d7a3d20b4f0e49dec4c48d8308faaabd7f
oktambek2003 <anvaralimnazarov84@gmail.com>
2022-11-03 14:55:23 +0500

    8-misol

--- bin/while_loop08.dart
+++ bin/while_loop08.dart
@@ -4,4 +4,20 @@
   
   
 */
-void main() {}
+String func(int a) {
+  int b = 0;
+  int c = 0;
+  String ans = "";
+  while (b <= a) {
+    if (b % 2 == 1) {
+      ans += b.toString();
+    }
+    b+=1;
+  }
+
+  return ans;
+}
+
+void main() {
+  print(func(10));
+}
