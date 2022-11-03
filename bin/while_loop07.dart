5958428a5252bbbca7f6f984d49629bbb778fe24
oktambek2003 <anvaralimnazarov84@gmail.com>
2022-11-03 14:17:46 +0500

    7-misol

--- bin/while_loop07.dart
+++ bin/while_loop07.dart
@@ -3,4 +3,31 @@
   Return odd and even numbers up to "a", whichever is smaller
   
 */
-void main() {}
+int func(int a) {
+  int b = 0;
+  int c = 0;
+  int d = 0;
+  int s = 0;
+  while (a > b) {
+    if (b % 2 == 1) {
+      c += b;
+    }
+    b += 1;
+  }
+  while (a > d) {
+    if (d % 2 == 0) {
+      s += d;
+    }
+    d += 1;
+  }
+  if (s > c) {
+    return s;
+  } else {
+    return c;
+  }
+}
+
+void main() {
+  print(func(10));
+}
+
